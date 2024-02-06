9c207c366ba6f951cdae24f5b881e5804cd415bd
Tomer Avgil <132493674+tomeravgil@users.noreply.github.com>
2024-02-06 16:47:50 -0500

    added files`

--- drivewise/src/App.js
+++ drivewise/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Contact from "./pages/Contact/Contact";
+import About from "./pages/About/About";
+import Services from "./pages/Services/Services";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/services" element={<Services/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
