74588956d9382f772843c9b9345c9d51718224c2
babcoh2 <babcoh2@rpi.edu>
2024-02-09 17:49:21 -0500

    added Big_box

---
+++ drivewise/src/Components/Big_box/Big_box.js
@@ -0,0 +1,27 @@
+import React from "react"; 
+import './Big_box.css'
+export default function Big_box({header_text, paragraph_text, button_text, button_link, link_text, link_link, image, flip}) {
+    const flipStyle = flip === 'true' ? 'double_box1' : 'double_box2';
+
+    return (
+         
+
+         <div className={`component ${flipStyle}`}>
+            <div className="image_box">
+                <img className="image" src={image} alt=""/>
+                
+            </div>
+            <div className="box">
+                <div>
+                    <header><h1>{header_text}</h1></header>
+                    <p>{paragraph_text}</p>
+                    <a href={button_link}><button >{button_text}</button> </a>
+                    <a className="text_link" href={link_link}>{link_text}</a>
+
+                </div>
+                
+            </div>
+        </div>
+
+    );
+}
