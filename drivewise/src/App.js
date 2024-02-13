748130f914f3bc33d72f574a8ef444e594dbf277
Tomer Avgil <132493674+tomeravgil@users.noreply.github.com>
2024-02-13 15:11:35 -0500

    developed using tailwind

--- drivewise/src/App.js
+++ drivewise/src/App.js
@@ -1,17 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import Contact from "./pages/Contact/Contact";
-import About from "./pages/About/About";
-import Services from "./pages/Services/Services";
+import Home from "./Pages/Home/Home";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/contact" element={<Contact/>} />
-        <Route path="/about" element={<About/>} />
-        <Route path="/services" element={<Services/>} />
       </Routes>
     </BrowserRouter>
   );
---
+++ drivewise/src/Pages/Home/Home.js
@@ -0,0 +1,8 @@
+import Carousel from "./../../Components/carousel";
+export default function Home(){
+    return (
+        <div>
+            <Carousel/>
+        </div>
+    );
+}
