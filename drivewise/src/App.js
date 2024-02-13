b2dc8db8fbc306b10273c5c680e3fbb3b8598640
babcoh2 <babcoh2@rpi.edu>
2024-02-13 16:44:18 -0500

    box component

--- drivewise/src/App.js
+++ drivewise/src/App.js
@@ -1,17 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import Contact from "./pages/Contact/Contact";
-import About from "./pages/About/About";
-import Services from "./pages/Services/Services";
+import Home from "./Pages/Home/Home";
+import About from "./Pages/About/About"
+// import Contact from "./pages/Contact/Contact";
+// import About from "./pages/About/About";
+// import Services from "./pages/Services/Services";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/contact" element={<Contact/>} />
+        <Route path="/Home" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        {/* <Route path="/contact" element={<Contact/>} />
         <Route path="/about" element={<About/>} />
-        <Route path="/services" element={<Services/>} />
+        <Route path="/services" element={<Services/>} /> */}
       </Routes>
     </BrowserRouter>
   );
---
+++ drivewise/src/Pages/About/About.js
@@ -0,0 +1,16 @@
+import React from "react"; 
+import './About.css'
+import Box from "../../Components/Box/Box";
+import City_map from "../../Images/map.jpg";
+
+function About() {
+
+    
+    return (
+        <body>
+            Hi
+            
+        </body>
+    );
+}
+export default About;
