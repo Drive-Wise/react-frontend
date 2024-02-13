c116bb9f863d22d308f1d64c992fcf51e2f6e689
Hayden Stimpson <stimph@rpi.edu>
2024-02-13 16:15:48 -0500

    added pages

--- drivewise/src/App.js
+++ drivewise/src/App.js
@@ -1,17 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import Contact from "./pages/Contact/Contact";
-import About from "./pages/About/About";
-import Services from "./pages/Services/Services";
+import Home from "./Pages/Home/Home";
+import About from "./Pages/About/About";
+import AccountPage from "./Pages/Account/Account";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/contact" element={<Contact/>} />
-        <Route path="/about" element={<About/>} />
-        <Route path="/services" element={<Services/>} />
+        <Route path="/Home" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Account" element={<AccountPage />} />
       </Routes>
     </BrowserRouter>
   );
