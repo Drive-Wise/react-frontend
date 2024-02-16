0d063b9ac7663bb11d4160bbb633d4178cf57eac
Hayden Stimpson <stimph@rpi.edu>
2024-02-16 17:00:52 -0500

    yay

--- drivewise/src/App.js
+++ drivewise/src/App.js
@@ -1,7 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home";
-import About from "./Pages/About/About";
-import AccountPage from "./Pages/Account/Account";
 
 function App() {
   return (
