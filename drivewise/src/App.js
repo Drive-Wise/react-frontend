470f2e0bf4c262de251f8fbd12f91180e6b856de
Tomer Avgil <132493674+tomeravgil@users.noreply.github.com>
2024-02-23 17:37:50 -0500

    added account page

--- drivewise/src/App.js
+++ drivewise/src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home";
-
+import Account from "./Pages/Account/Account";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Home />} />
         <Route path="/Home" element={<Home />} />
+        <Route path="/Account" element={<Account />} />
       </Routes>
     </BrowserRouter>
   );
