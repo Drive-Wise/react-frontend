d95edc372f090be421200db1aeb5aeaa83d3751d
jdechiro05 <jackdech05@gmail.com>
2024-03-12 16:37:30 -0400

    added

--- drivewise/src/App.js
+++ drivewise/src/App.js
@@ -1,17 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import Contact from "./pages/Contact/Contact";
-import About from "./pages/About/About";
-import Services from "./pages/Services/Services";
+import Home from "../src/Pages/Home/Home";
+import Founders from "../src/Pages/Founders/Founders"
+
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Home />} />
         <Route path="/home" element={<Home />} />
-        <Route path="/contact" element={<Contact/>} />
-        <Route path="/about" element={<About/>} />
-        <Route path="/services" element={<Services/>} />
+        <Route path="/Founders" element={<Founders />} />
       </Routes>
     </BrowserRouter>
   );
