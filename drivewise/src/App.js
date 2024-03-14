692dc288190bc6539371f377689e4b0cb82164be
Hayden Stimpson <stimph@rpi.edu>
2024-03-13 20:21:13 -0400

    Added about page with dummie info

--- drivewise/src/App.js
+++ drivewise/src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import Account from "./Pages/Account/Account";
+import About from "./Pages/About/About";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Home />} />
         <Route path="/Home" element={<Home />} />
+        <Route path="/About" element = {<About />} />
         <Route path="/Account" element={<Account />} />
       </Routes>
     </BrowserRouter>
