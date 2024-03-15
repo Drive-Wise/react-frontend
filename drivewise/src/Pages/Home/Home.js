43d83cf252d92d65665b95e8addfe51d7d647510
Hayden Stimpson <stimph@rpi.edu>
2024-03-15 12:23:25 -0400

    changed button highlight and linking

--- drivewise/src/Pages/Home/Home.js
+++ drivewise/src/Pages/Home/Home.js
@@ -76,7 +76,7 @@ function Hero() {
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
                 href="/SignUp"
-                className="rounded-md bg-title px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-400focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="rounded-md bg-title px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Get started
               </a>
